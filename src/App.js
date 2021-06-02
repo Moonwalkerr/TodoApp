c5b0ee533f425b8ef79603a62b778e94c333cbe3
Moonwalker <abhisk.mhra@gmail.com>
2021-06-02 23:01:07 +0530

    imported inputfield to app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { InputField } from "./comps";
 
 function App() {
   return (
     <div className="App">
       <h2>TODO App by Abhishek Mishra !</h2>
+      <InputField />
     </div>
   );
 }
