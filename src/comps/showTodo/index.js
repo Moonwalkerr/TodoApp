af981363130c6ac659ba7fe53b81785f800c6769
Moonwalker <abhisk.mhra@gmail.com>
2021-06-03 19:42:28 +0530

    Todos display card

---
+++ src/comps/showTodo/index.js
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { AppContext } from "../../context/context";
+
+const showTodo = (todo, id) => {
+  const [todos, setTodos] = useContext(AppContext).todos;
+
+  // delete todo handler
+  const delTodo = (id) => {
+    console.log(id);
+  };
+
+  return (
+    <div className="showTodo">
+      <h3>{todo}</h3>
+      <button onClick={delTodo}>Del</button>
+    </div>
+  );
+};
+
+export default showTodo;
